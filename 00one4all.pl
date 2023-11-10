7a02f1723b6021e651cf855332221a31b0b78855
jushinpon <jushinpon@gmail.com>
2023-11-10 14:18:15 +0800

    modify special character for mp files

--- 00one4all.pl
+++ 00one4all.pl
@@ -7,7 +7,7 @@ system("rm -rf cifs cif2data data2QE4MatCld QEinByMatCld QE_trimmed");
 
 print "1. getting all cif files from materials project into cifs \n";
 system("python mp_get_cif.py");
-die;
+
 print "2. converting all cif files to data files into cif2data \n";
 system("perl cif2data.pl");
 
