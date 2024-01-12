66a285197549d8c6f6fd094a95646c24ebe07fef
jushinpon <jushinpon@gmail.com>
2024-01-12 12:35:22 +0800

    remove orth-cell for atomsk

--- cif2data_orth.pl
+++ cif2data_orth.pl
@@ -7,6 +7,7 @@ use POSIX;
 unlink "cif_summary.txt";
 my $maxNum = 64;#maximum number allowed in cif files
 open(my $DATA, ">cif_summary.txt");
+open(my $DATA_orth, ">cif_summary_orth.txt");
 my $currentPath = getcwd();
 my @ciffiles = `find $currentPath/cifs -name "*.cif"`;
 #my @ciffiles =("/home/jsp/QE_from_MatCld/cifs/Al3(BRu2)2_mp-541849.cif");
@@ -31,7 +32,7 @@ for my $cif (@ciffiles){
     chomp ($data_path, $data_name);
     
     my $output = "cif2data/$data_name.lmp";
-#    my $output_orth = "cif2data/$data_name-orth.lmp";
+    my $output_orth = "cif2data/$data_name-orth.lmp";
     my $outputdata = "cif2data/$data_name.data";
     unlink "$output";
     #system("atomsk $cif -alignx -unskew -wrap $output");
@@ -39,17 +40,14 @@ for my $cif (@ciffiles){
     my $atomnum = `grep atoms $output|awk '{print \$1}'`;
     $atomnum =~ s/^\s+|\s+$//g;
     print "$atomnum , $maxNum, $cif\n";
-    if($atomnum < 4){
+    if($atomnum <= 4){
         print $DATA "atom number of $cif (current $atomnum) <= 4, using 2 2 1 supercell\n";
         unlink "$output";
         system("atomsk $cif -alignx -unskew -duplicate 2 2 1 $output");
-        system("mv $output $outputdata");
-
-    }elsif($atomnum >= 4 and $atomnum <= $maxNum){
+    }elsif($atomnum > 4 and $atomnum <= $maxNum){
         print $DATA "***atom number of $cif (current $atomnum) <= $maxNum and > 4,no supercell is used.\n";
         unlink "$output";         
         system("atomsk $cif -alignx -unskew  $output");
-        system("mv $output $outputdata");
     }
     else{
         print $DATA "!!!!atom number of $cif (current $atomnum) >= $maxNum, not use this cif\n";        
@@ -57,11 +55,32 @@ for my $cif (@ciffiles){
         unlink "$output";     
         next;
     }
-
+    `atomsk $output -orthogonal-cell $output_orth`;
+    my $newatomnu = `grep atoms $output_orth`;
+    $newatomnu =~ s/^\s+|\s+$//g;
+    if($newatomnu <= $maxNum ){
+        print $DATA_orth "!!!!atom number of $newatomnu for $outputdata\n";        
+        
+        my @temp = `cat $output_orth`;
+        map { s/^\s+|\s+$//g; } @temp;
+        my $temp = join("\n",@temp);
+        chomp $temp;
+        unlink "$output";
+        unlink "$output_orth";
+        unlink "$outputdata";
+        open(my $DATA1, ">$outputdata");
+        print $DATA1 "$temp\n";
+        close($DATA1); 
+    }
+    else{
+        unlink "$output_orth";
+        unlink "$output";
+    }  
 }
 
 close($DATA);
-print "\n***Pleae check cif_summary.txt or the following content of cif_summary.txt:\n";
-print "\n***printing cif_summary.txt!!!\n";
-system("cat ./cif_summary.txt");
+close($DATA_orth);
+print "\n***Pleae check cif_summary_orth.txt or the following content of cif_summary.txt:\n";
+print "\n***printing cif_summary_orth.txt!!!\n";
+system("cat ./cif_summary_orth.txt");
 #print "\n\n*** If the above is empty, no cif is skipped or using supercell for data file.\n";
